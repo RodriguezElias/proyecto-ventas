368990efef73179fb9c9db6e6bc4434593c4911f
Elias Rodriguez <elrodriguez@dlatv.net>
2019-11-26 18:06:36 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login de Usuario</title>
+    <link rel="stylesheet" type="text/css" href="librerias/bootstrap/css/bootstrap.css">
+    <script src="librerias/jquery-3.2.1.min.js"></script>
+
+</head>
+<body style="background-color: gray">
+<br><br><br>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <div class="panel panel-primary">
+                    <div class="panel panel-heading">Sistema de ventas de Hardware</div>
+                        <div class="panel panel-body">
+
+<p>
+    <img src="img/icono-1.png" width="300px" heigth="300px" >
+</p>
+<form id="frmLogin">
+    <label>Usuario</label>
+        <input type="text" class="form-control input-sm" name="usuario" id="usuario">
+    <label>Password</label>
+        <input type="password" class="form-control input-sm" name="password" id="password">
+    <p></p>
+    <span class="btn btn-primary ">Entrar</span>
+    <a href="registro.php" class="btn btn-danger">Registrar</a>
+</form>
+
+</div>
+</div>
+</div>
+        <div class="col-sm-4"></div>
+</div>
+</div>
+    
+</body>
+</html>
