4f68e72597ed3aaf5a41d4b5a83fe25fedfc5f4b
NachoIfts16 <elalba98@hotmail.com>
2019-11-28 13:02:21 -0300

    Maquetacion modulo clientes
    Creación de clientes.

--- vistas/clientes.php
+++ vistas/clientes.php
@@ -18,9 +18,62 @@ if(isset($_SESSION['usuario'])){
 		<?php require_once "menu.php" ?>
 	</head>
 	<body>
+		<div class="container">
+		<h1>Clientes</h1>
+			<div class="col-sm-4">
+				<form id="frmClientes">
+					<label>Nombre</label>
+					<input type="text" class="form-control input-sm" id="nombre" name="nombre">
+					<label>Apellido</label>
+					<input type="text" class="form-control input-sm" id="apellidos" name="apellidos">
+					<label>Direccion</label>
+					<input type="text" class="form-control input-sm" id="direccion" name="direccion">
+					<label>Mail</label>
+					<input type="text" class="form-control input-sm" id="mail" name="mail">
+					<label>Telefono</label>
+					<input type="text" class="form-control input-sm" id="telefono" name="telefono">
+					<p></p>
+					<span class="btn btn-primary" id="btnAgregarCliente">Agregar</span>
+				
+				</form>
+			</div>
+			<div class="col-sm-8">
+				<div id="tablaClientesLoad"></div>
+			</div>
+				
+		</div>
 
 	</body>
 	</html>
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#tablaClientesLoad').load("clientes/tablaClientes.php");
+			$('#btnAgregarCliente').click(function(){
+				vacios=validarFormVacio('frmClientes');
+				if(vacios > 0){
+					alertify.alert("Debes llenar todos los campos!!");
+					return false;
+				}
+				datos=$('#frmClientes').serialize();
+				$.ajax({
+					type:"POST",
+					data:datos,
+					url:"../procesos/clientes/agregaCliente.php",
+					success:function(r){
+						if(r==1){
+					//esta linea nos permite limpiar el formulario al insetar un registro
+					$('#frmClientes')[0].reset();
+					$('#tablaClientesLoad').load("clientes/tablaClientes.php");
+					alertify.success("Agregado con exito :D");
+				}else{
+					alertify.error("No se pudo agregar");
+				}
+			}
+		});
+			});
+		});
+	</script>
 
 	<?php
 
