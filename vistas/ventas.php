5f50f3c9b1c8d128d55e411e6f6901f2cd0ba2a0
NachoIfts16 <elalba98@hotmail.com>
2019-11-28 14:03:37 -0300

    Maquetacion modulo ventas
    Creacion de la seccion ventas

--- vistas/ventas.php
+++ vistas/ventas.php
@@ -18,9 +18,51 @@ if(isset($_SESSION['usuario'])){
 		<?php require_once "menu.php" ?>
 	</head>
 	<body>
-
+		<div class="container">
+			<h1>Venta de productos</h1>
+			<div class="row">
+				<div class="col-sm-12">
+					<span class="btn btn-default" id="ventaProductosBtn">Vender producto</span>
+					<span class="btn btn-default" id="ventasHechasBtn">Ventas hechas</span>
+				</div>
+			</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<div id="ventaProductos"></div>
+						<div id="ventasHechas"></div>
+					</div>
+				</div>
+		</div>
 	</body>
 	</html>
+<!--cuando se haga click
+	1°esconde la seccion
+	2°carga la seccion
+	3°Muestra -->
+	<script type="text/javascript">
+		$(document).ready(function(){
+			
+			$('#ventaProductosBtn').click(function(){
+				esconderSeccionVenta();
+				$('#ventaProductos').load('ventas/ventasDeProductos.php');
+				$('#ventaProductos').show();
+			});
+
+			$('#ventasHechasBtn').click(function(){
+				esconderSeccionVenta();
+				$('#ventasHechas').load('ventas/ventasyReportes.php');
+				$('#ventasHechas').show();
+			});
+
+		});
+
+		function esconderSeccionVenta(){
+			$('#ventaProductos').hide();
+			$('#ventasHechas').hide();
+
+		};
+
+	</script>
 
 	<?php
 
