533dfa961cd0ec33324c6fd4e639f159c9c16f53
NachoIfts16 <elalba98@hotmail.com>
2019-11-28 12:00:16 -0300

    Maquetacion modulo usuarios
    Creación de Usuarios

--- vistas/usuarios.php
+++ vistas/usuarios.php
@@ -18,9 +18,62 @@ if(isset($_SESSION['usuario'])){
 		<?php require_once "menu.php" ?>
 	</head>
 	<body>
-
+		<div class="container">
+			<h1>Administrar Usuarios</h1>
+			<div class="row">
+				<div class="col-sm-4">
+<!--Casi el mismo form q el registro -->				
+				<form id="frmRegistro">
+                     <label>Nombre</label>
+                     <input type="text" class="form-control input-sm" name="nombre" id="nombre">
+                     <label>Apellido</label>
+                     <input type="text" class="form-control input-sm" name="apellido" id="apellido">
+                     <label>Usuario</label>
+                     <input type="text" class="form-control input-sm" name="usuario" id="usuario">
+                     <label>Password</label>
+                     <input type="password" class="form-control input-sm" name="password" id="password">
+                     <p></p>
+                     <span class="btn btn-primary" id="registro">Registrar</span>                    
+                 </form>
+				</div>
+				<div class="col-sm-7">
+				<div id="tablaUsuariosLoad"></div>
+				
+				</div>
+			</div>
+		</div>
 	</body>
 	</html>
+<!-- casi el mismo script q en registro-->
+	<script type="text/javascript">
+    $(document).ready(function(){
+		$('#tablaUsuariosLoad').load('usuarios/tablaUsuarios.php');
+        $('#registro').click(function(){
+
+            vacios = validarFormVacio('frmRegistro');
+            if(vacios > 0){
+                alertify.alert("Debes llenar todos lo campos!");
+                return false;
+            }
+
+            datos=$('#frmRegistro').serialize();
+            $.ajax({
+                type:"POST",
+                data:datos,
+                url:"../procesos/regLogin/registrarUsuario.php",
+                success:function(r){
+
+                    if(r==1){
+                        alertify.success("Agregado con exito");
+                    }else{
+                        alertify.error("Fallo al agregar");
+                    }
+                }
+            });
+        });
+
+    });
+</script>	
 
 	<?php
 
