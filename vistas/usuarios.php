8433cb3ede1d999c5f1f230a1cad0e7d77b9ec54
NachoIfts16 <elalba98@hotmail.com>
2019-12-01 15:00:44 -0300

    Editar usuario

--- vistas/usuarios.php
+++ vistas/usuarios.php
@@ -100,8 +100,15 @@ if(isset($_SESSION['usuario']) and $_SESSION['usuario'] =='admin'){
 		$.ajax({
 			type:"POST",
 			data:datos,
-			url:"../procesos/",
+			url:"../procesos/usuarios/actualizaUsuario.php",
 			success:function(r){
+                
+                if(r==1){
+							$('#tablaUsuariosLoad').load('usuarios/tablaUsuarios.php');
+							alertify.success("Actualizado con exito");
+						}else{
+							alertify.error("No se pudo actualizar");
+						}
 			}
 		});
 	});
